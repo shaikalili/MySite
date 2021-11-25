994dd3fa5d01fd02de162d22c7335fe89158e40f
shaikalili <shookashai.sk@gmail.com>
2021-11-25 15:49:23 +0200

    mobile commit

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 var scroll = new SmoothScroll('a[href*="#"]');
 var mobileNav = document.getElementById("mobile-nav");
 
-
 document.getElementById("menu_checkbox").addEventListener("click", () => {
     mobileNav.classList.toggle("mystyle");
 });
+
+for (let child of mobileNav.children) {
+    child.addEventListener("click", () => {
+        mobileNav.classList.remove("mystyle");
+    });
+}
